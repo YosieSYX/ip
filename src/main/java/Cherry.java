e9abdbedb8bbf7e7c1047a88f018d8e07ec7f89f
YosieSYX <E1300499@U.NUS.EDU>
2025-01-22 22:42:25 +0800

    Implemented Level-3

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Cherry {
     public static void main(String[] args) {
@@ -6,8 +7,7 @@ public class Cherry {
         System.out.println("What can I do for you today?\n" );
         Scanner scanner = new Scanner(System.in);
         String input;
-        String[] tasks = new String[100];
-        int count = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
             input = scanner.nextLine();
 
@@ -15,21 +15,35 @@ public class Cherry {
                 System.out.println("Bye! See you next time!");
                 break;
             }
-            if (input.equalsIgnoreCase("list")) {
-                if(count == 0) {
-                    System.out.println("There's nothing in the list now");
-                }
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-            } else {
-                if (count < 100) {
-                    tasks[count] = input;
-                    count++;
-                    System.out.println("added: " + input);
+            else if (input.equalsIgnoreCase("list")) {
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks in the list.");
                 } else {
-                    System.out.println("Sorry, You've hit my capacity!");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
                 }
+            }
+            else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+            }
+            else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                Task task = tasks.get(taskNumber - 1);
+                task.markAsUndone();
+                System.out.println("Nice! I've marked this task as not done yet:");
+                System.out.println("  " + task);
+            }
+            else {
+                Task task = new Task(input);
+                tasks.add(task);
+                System.out.println("added: " + input);
             }
         }
 
