5892892898cec298432b1a638255e560467382fd
YosieSYX <E1300499@U.NUS.EDU>
2025-01-22 23:37:51 +0800

    Implemented Level-4

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Cherry {
     public static void main(String[] args) {
+        int count = 0;
         System.out.println("Hello from Cherry!\n" );
         System.out.println("What can I do for you today?\n" );
         Scanner scanner = new Scanner(System.in);
@@ -41,9 +42,29 @@ public class Cherry {
                 System.out.println("  " + task);
             }
             else {
-                Task task = new Task(input);
-                tasks.add(task);
-                System.out.println("added: " + input);
+                if (input.startsWith("todo")) {
+                    Task task = new ToDos(input);
+                    tasks.add(task);
+                    System.out.println("added: " + input);
+                }
+                else if (input.startsWith("deadline")) {
+                    String[] parts = input.split(" /by ");
+                    String time = parts[1];
+                    Task task = new Deadline(input, time);
+                    tasks.add(task);
+                    System.out.println("added: " + input);
+                }
+                else if (input.startsWith("event")) {
+                    String[] parts1 = input.split(" /from ");
+                    String start = parts1[1].split(" /to ")[0];
+                    String[] parts2 = input.split(" /to ");
+                    String end = parts2[1];
+                    Task task = new Events(input, start, end);
+                    tasks.add(task);
+                    System.out.println("added: " + input);
+                }
+                count ++;
+                System.out.println("Now you have " + count + " tasks in the list!");
             }
         }
 
