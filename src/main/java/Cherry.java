55476580792feb1fce1a2ff40b4dd3071020a98d
YosieSYX <E1300499@U.NUS.EDU>
2025-01-22 20:16:48 +0800

    Implemented level-2

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -6,14 +6,31 @@ public class Cherry {
         System.out.println("What can I do for you today?\n" );
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] tasks = new String[100];
+        int count = 0;
         while (true) {
             input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("Bye!");
+                System.out.println("Bye! See you next time!");
                 break;
             }
-
+            if (input.equalsIgnoreCase("list")) {
+                if(count == 0) {
+                    System.out.println("There's nothing in the list now");
+                }
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                if (count < 100) {
+                    tasks[count] = input;
+                    count++;
+                    System.out.println("added: " + input);
+                } else {
+                    System.out.println("Sorry, You've hit my capacity!");
+                }
+            }
         }
 
         scanner.close();
