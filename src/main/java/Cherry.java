29b587a1947eb6f103ef0189064dc39c4d6fb2cf
YosieSYX <E1300499@U.NUS.EDU>
2025-01-22 20:14:37 +0800

    Implemented level-1

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -4,7 +4,20 @@ public class Cherry {
     public static void main(String[] args) {
         System.out.println("Hello from Cherry!\n" );
         System.out.println("What can I do for you today?\n" );
-        System.out.println("Bye!");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye!");
+                break;
+            }
+
+        }
+
+        scanner.close();
+
         System.exit(0);
     }
 }
