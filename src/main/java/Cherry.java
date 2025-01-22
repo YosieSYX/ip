3e66bbd01eedabb304bb9391e589c2e8b01478c5
YosieSYX <E1300499@U.NUS.EDU>
2025-01-22 20:13:14 +0800

    Implemented level-0

--- src/main/java/Cherry.java
+++ src/main/java/Cherry.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Cherry {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
-        int count = 0;
         System.out.println("Hello from Cherry!\n" );
         System.out.println("What can I do for you today?\n" );
         System.out.println("Bye!");
