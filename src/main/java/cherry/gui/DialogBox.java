b4f5d453687982663b0bc087da5d8bfb3771fa56
YosieSYX <e1300499@u.nus.edu>
2025-02-20 10:33:09 +0800

    Use AI
    Use AI to generate JavaDoc comment.

--- src/main/java/cherry/gui/DialogBox.java
+++ src/main/java/cherry/gui/DialogBox.java
@@ -7,19 +7,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 /**
@@ -50,7 +43,7 @@ public class DialogBox extends HBox {
         clip.setCenterX(radius);
         clip.setCenterY(radius);
         displayPicture.setClip(clip);
-        
+
     }
 
 
